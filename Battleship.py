212526424f4c831660d65d4fbe3a0ac00ab64971
NiravChotai <niravchotai.ce@gmail.com>
2014-11-24 11:51:37 +0530

    Updated comments

--- Battleship.py
+++ Battleship.py
@@ -1,12 +1,12 @@
 '''
 Created on Sep 12, 2014
+@author: nirav.chotai
+
 In this project we build a simplified, 
 one-player version of the classic board game Battleship! 
 In this version of the game, 
 there will be a single ship hidden in a random location on a 5x5 grid. 
 The player will have 4 guesses to try to sink the ship.
-
-@author: nirav.chotai
 '''
 from random import randint
 
